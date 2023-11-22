2c922994efc56b0344add17794f13d4fcaec2646
maheshkoheda <maheshkoheda5@gmail.com>
2023-11-22 14:55:29 -0500

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call() {
-  sh 'echo Hi From DevOps Team, checking the edit features now'
+  sh 'echo Hi From DevOps Team, checking the edit features now, deleted mvnbuild to check this success'
 }
